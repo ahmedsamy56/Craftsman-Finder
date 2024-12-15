361ea526d693705d04cb99acfa4956d4ab10d3e5
Ahmed <ahmedsamylike@gmail.com>
2024-12-16 00:25:08 +0200

    jobreqest

--- CraftsmanFinder/CraftsmanFinder.DataAccess/obj/Debug/net9.0/CraftsmanFinder.DataAccess.AssemblyInfo.cs
+++ CraftsmanFinder/CraftsmanFinder.DataAccess/obj/Debug/net9.0/CraftsmanFinder.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CraftsmanFinder.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7467a42209b4c39950999a475a2d06e540d75289")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92ef05e367d06e8a37a1adf8592969e392a673d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CraftsmanFinder.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CraftsmanFinder.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ CraftsmanFinder/CraftsmanFinder.Web/Areas/Admin/Controllers/JobRequestController.cs
@@ -0,0 +1,37 @@
+﻿using CraftsmanFinder.DataAccess.Repository.IRepository;
+using CraftsmanFinder.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CraftsmanFinder.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = $"{SD.AdminRole}")]
+    public class JobRequestController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public JobRequestController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var jobRequests = await _unitOfWork.JobRequests.GetAllAsync(Includeword: "ApplicationUser,Category");
+            return View(jobRequests);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var review = await _unitOfWork.JobRequests.GetFirstorDefaultsync(x => x.Id == id);
+            if (review != null)
+            {
+                await _unitOfWork.JobRequests.DeleteJobRequestWithDependenciesAsync(id);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
